df2cc1d3f5d08530b361fd69c324eabb1e290e7e
Shiv-2018 <shivindra2018@gmail.com>
2025-06-30 16:38:19 +0530

    Commited

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ import postRouter from "./routes/post.routes.js";
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173", // Adjust this to your frontend's URL
+    origin: "https://dayblog.vercel.app", // Adjust this to your frontend's URL
     credentials: true, // Allow cookies to be sent with requests
   })
 );
